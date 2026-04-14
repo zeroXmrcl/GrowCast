5fbc2859d0c50ec10732926bc9486f538046538a
0xmrcl <219437614+zeroXmrcl@users.noreply.github.com>
2026-04-14 13:01:40 +0200

    Add security headers to Next.js config.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    async headers() {
+        return [
+            {
+                source: "/:path*",
+                headers: [
+                    {
+                        key: "X-Frame-Options",
+                        value: "DENY",
+                    },
+                    {
+                        key: "X-Content-Type-Options",
+                        value: "nosniff",
+                    },
+                    {
+                        key: "Strict-Transport-Security",
+                        value: "max-age=31536000; includeSubDomains",
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
